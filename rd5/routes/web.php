01cf07c4f88dea716d93f4e57752f0b393ccf111
alanwang1207 <alan_wang@mail.chungyo.net>
2020-09-03 15:15:23 +0800

    設定路由網址

--- rd5/routes/web.php
+++ rd5/routes/web.php
@@ -13,6 +13,36 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//首頁
+Route::get('/', 'HomeController@indexPage');
+
+//使用者
+Route::group(['prefix' => 'user'], function () {
+    //使用者驗證
+    Route::group(['prefix' => 'auth'], function () {
+        Route::get('/sign-up', 'UserAuthController@signUpPage');
+        Route::post('/sign-up', 'UserAuthController@signUpProcess');
+        Route::get('/sign-in', 'UserAuthController@signInPage');
+        Route::post('/sign-in', 'UserAuthController@signInProcess');
+        Route::get('/sign-out', 'UserAuthController@signOut');
+    });
 });
+
+//商品
+Route::group(['prefix' => 'merchandise'], function () {
+    Route::get('/', 'MerchandiseController@merchandiseListPage');
+    Route::get('/create', 'MerchandiseController@merchandiseCreateProcess');
+    Route::get('/manage', 'MerchandiseController@merchandiseManageListPage');
+
+    //指定商品
+    Route::group(['prefix' => '{merchandise_id}'], function () {
+        Route::get('/', 'MerchandiseController@merchandiseItemPage');
+        Route::get('/edit', 'MerchandiseController@merchandiseItemEditPage');
+        Route::put('/', 'MerchandiseController@merchandiseItemUpdateProcess');
+        Route::get('/buy', 'MerchandiseController@merchandiseItemBuyProcess');
+    });
+});
+
+//交易
+Route::get('transaction', 'TransactionController@transactionListPage');
+
