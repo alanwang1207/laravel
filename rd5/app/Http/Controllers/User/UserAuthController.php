f6fd4841240ed5cd6ada1fc88aa6fc94e9431059
alanwang1207 <alan_wang@mail.chungyo.net>
2020-09-03 15:30:50 +0800

    建立註冊會員控制器

---
+++ rd5/app/Http/Controllers/User/UserAuthController.php
@@ -0,0 +1,15 @@
+<?php
+
+//命名空間
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+
+class UserAuthController extends Controller {
+
+    //註冊頁
+    public function signUpPage(){
+
+    }
+}
+?>
