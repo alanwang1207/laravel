87c36b6cd309a4dc49d1fc98a0a2696f6200a785
alanwang1207 <alan_wang@mail.chungyo.net>
2020-09-03 15:43:33 +0800

    建立註冊頁模板

---
+++ rd5/resources/views/auth/signUp.blade.php
@@ -0,0 +1,18 @@
+<?php
+
+//命名空間
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class UserAuthController extends Controller{
+    //註冊頁
+    public function signUpPage(){
+        $binding = [
+            'title' => '註冊',
+        ];
+        return view('auth.signUp', $binding);
+    }
+}
+
+
